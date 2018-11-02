77244ed7597b7fa11caf763a907f6fb21809c7dc
Dmitry Baryshnikov <bishop.dev@gmail.com>
2018-11-02 17:25:05 +0300

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -33,7 +33,7 @@ sys.path.append(os.path.abspath('sphinxext'))
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.todo',
-    'sphinx.ext.pngmath',
+    'sphinx.ext.imgmath',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
